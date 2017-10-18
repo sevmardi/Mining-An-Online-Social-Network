00667651bebd599340289154599a8581e7ecc3d5
Sevak Mardirosian <smardirosians@gmail.com>
2017-10-18 21:55:58 +0200

    few changes

--- Assignment2/Exercise3/ex3_02.py
+++ Assignment2/Exercise3/ex3_02.py
@@ -4,47 +4,64 @@ import _pickle as pic
 import csv
 import numpy as np
 from graph_tool.all import *
-reload(sys)
+
 sys.setdefaultencoding("UTF-8")
 
 data = 'data/twitter-small.in'
 
+
 def number_of_edges(graph, title):
-	print('Number of Edges')
-	print(title + ': ' + str(nx.number_of_edges(graph))
-	print('\n')
+    print('Number of Edges')
+    print(title + ": " + str(nx.number_of_edges(graph)))
+    print('\n')
+
 
 def number_of_nodes(graph, title):
-	print("Number of Nodes in the network")
-	print(title + ": " + str(nx.number_of_nodes(graph)))
-	print('\n')
+    print("Number of Nodes in the network")
+    print(title + ": " + str(nx.number_of_nodes(graph)))
+    print('\n')
+
 
 def network_density(graph, title):
-	print('Network Density')
-	print(title + ': ' + str(nx.density(graph)))
-	print('\n')
+    print('Network Density')
+    print(title + ': ' + str(nx.density(graph)))
+    print('\n')
 
-def in_degree_distribution(DiGraph, title, fileName):
-	in_degrees = DiGraph.in_degree().values()
-	bin_count = np.bincount(np.array(in_degrees))
-	dump(bin_count, in_degrees)
-	print("In Degree Distribution " + title + " \n")
-	print(bin_count)
-	print("\n")
 
-#TODO
-def main():
-	pass
+def in_degree_distribution(DiGraph, title, filename):
+    in_degrees = DiGraph.in_degree().values()
+    bin_count = np.bincount(np.array(in_degrees))
+    dump(bin_count, filename)
+    print("In Degree Distribution " + title + " \n")
+    print(bin_count)
+    print("\n")
 
-#TODO
+
+def out_degree_distribution(DiGraph, title, filename):
+    out_degree = DiGraph.out_degree().values()
+    bin_count = np.bincount(np.array(out_degree))
+    dump(bin_count, filename)
+    print("Out Degree Distribution " + title + " \n")
+    print(bin_count)
+    print("\n")
+
+
+# TODO
 def parse_file_to_digraph(filename):
-	pass
+    pass
 
-#TODO
+# TODO
+
+
 def parse_file_to_gtdigraph(filename):
 
-	pass
+    pass
 
 
 def dump(picle, filename):
-    pic.dump(picle , open(filename,'wb'))
+    pic.dump(picle, open(filename, 'wb'))
+
+
+# TODO
+def main():
+    pass
